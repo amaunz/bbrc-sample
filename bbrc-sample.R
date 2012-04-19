5972008cf98983288006f5c9f4f1f62540940750
Andreas Maunz <andreas@maunz.de>
2012-04-19 14:17:54 +0200

    Completed, but with inefficient join

--- bbrc-sample.R
+++ bbrc-sample.R
@@ -1,40 +1,54 @@
-library('RCurl')
-library('doMC')
+suppressPackageStartupMessages(library('RCurl'))
+suppressPackageStartupMessages(library('doMC'))
 registerDoMC()
 
 SERVER="http://toxcreate3.in-silico.ch:8082"
 BBRC=paste(SERVER,"algorithm","fminer","bbrc",sep='/')
-num.boots=100
+BOOTS=100
 
 
 # # # Main function
 
-bootBbrc = function(datasetUri, numboots=num.boots) {
+bootBbrc = function(datasetUri, numboots=BOOTS) {
 
   # load dataset
   ds = getdataset(datasetUri)
   n = dim(ds)[1]
+  cat(paste("Full set size:",n,"\n"))
 
   # main loop
-  foreach(j=1:numboots, .combine=mergelists) %dopar% {
+  bb = foreach(j=1:numboots, .combine=mergelists) %dopar% {
     idx=drawsample(1:n)
     sample=ds[idx,]
-    task=postdataset(sample,tfprefix=paste("boot_bbrc_sample",j,sep=""))
-    while(1) { sampleUri = gettask(task); if (sampleUri != task) break; }
+
+    task=postdataset(sample,tempfileprefix=paste("boot_bbrc_sample_",j,"_",sep=""))
+    sampleUri = getresult(task)
+
     task=postrequest(BBRC,list(dataset_uri=sampleUri))
-    while(1) { sampleFeaturesUri = gettask(task); if (sampleFeaturesUri != task) break; }
+    sampleFeaturesUri = getresult(task)
+    print(sampleFeaturesUri)
+
     sampleFeatures = getdataset(sampleFeaturesUri)
+    print(dim(sampleFeatures))
     sampleFeatures["SMILES"]=NULL
-    apply(sampleFeatures,2,function(x) sum(x))
+    sampleFeatures=apply(sampleFeatures,2,function(x) sum(x))
     as.list(sampleFeatures)
   }
+  bb
 
 }
 
 # merges 2nd list to 1st and returns result
 # suitable for .combine
 mergelists = function(x,xn) {
-  for(idx in names(xn)) { x[[idx]] = c( x[[idx]], xn[[idx]] ) }
+  padlen = length(x[[1]])
+  print("PAD UNK")
+  for (n in names(x)[!names(x) %in% names(xn)])  xn[[n]] = 0 
+  print("PAD NEW")
+  for (n in names(xn)[!names(xn) %in% names(x)]) xn[[n]] = c(rep(0,padlen), xn[[n]])
+  print("MERGE")
+  for (idx in names(xn)) { x[[idx]] = c( x[[idx]], xn[[idx]] ) } 
+  print("DONE")
   x
 }
 
@@ -43,11 +57,14 @@ drawsample = function(x,size=length(x)) {
   sample(x,size=size,replace=T)
 }
 
-# get task
-gettask = function(uri) {
-  Sys.sleep(0.75)
-  uri = getrequest(uri,"text/uri-list")
-  #substr(uri, 1, nchar(uri)-1) # remove trailing newline
+# get dataset from task
+getresult= function(uri) {
+  while(1) { 
+    Sys.sleep(0.5)
+    uri = getrequest(uri,"text/uri-list")
+    if (length(grep("dataset",uri))>0) break
+  }
+  uri
 }
 
 
@@ -71,18 +88,18 @@ getdataset = function(uri) {
 }
 
 # POST a dataset (CSV)
-postdataset = function(x,tfprefix="R_") {
-  tf=tempfile(pattern=paste(tfprefix, Sys.getpid(), sep=""))
+postdataset = function(x,tempfileprefix="R_") {
+  tf=tempfile(pattern=paste(tempfileprefix, Sys.getpid(), sep=""))
   print(tf)
   tryCatch({
     write.csv(x=x,file=tf,row.names=F,quote=F,na='')
     task=postForm(paste(SERVER,"dataset",sep='/'), file=fileUpload(filename=tf,contentType="text/csv"), .checkParams=F )
   }, finally = {
-    #unlink(tf)
+    unlink(tf)
   })
   task
 }
 
 
 # # # start
-bootBbrc("http://toxcreate3.in-silico.ch:8082/dataset/923",1)
+bootBbrc("http://toxcreate3.in-silico.ch:8082/dataset/923",numboots=100)
